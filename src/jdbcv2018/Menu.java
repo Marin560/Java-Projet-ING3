672d6d8034f808fa3aa5f3d0b3b5c0fb919d88a9
Unknown <roxane.batigny@gmail.com>
2018-04-16 23:35:40 +0200

    Avancée du Menu, affichage des boutons

--- src/jdbcv2018/Menu.java
+++ src/jdbcv2018/Menu.java
@@ -11,9 +11,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener; 
 import java.awt.event.ItemEvent; 
 import java.awt.event.ItemListener; 
+import java.sql.Connection;
 import javax.swing.JButton; 
 import javax.swing.JFrame; 
 import javax.swing.JPanel; 
+import javax.swing.JTable;
 
 /**
  *
@@ -25,9 +27,12 @@ public class Menu extends JFrame implements ActionListener, ItemListener{
     private JButton Recherche, MaJ, Reporting, Connexion; 
     private JPanel p0, p1, p2, p3; 
     
+    public JTable jtable;
+    public String namebdd;
+    
   public Menu(){ 
        // creation par heritage de la fenetre 
-        super("Bienvenue dans logiciel de Gestion du Centre Hospitalier"); 
+        super("Bienvenue dans le logiciel de Gestion du Centre Hospitalier"); 
  
         // mise en page (layout) de la fenetre visible 
         setLayout(new BorderLayout()); 
@@ -41,41 +46,59 @@ public class Menu extends JFrame implements ActionListener, ItemListener{
         MaJ = new JButton ("Mise à Jour des données"); 
         Reporting = new JButton ("Reporting"); 
         Connexion = new JButton("Connexion"); 
+        
          
         // Création de panneaux  
         p0 = new JPanel(); 
         p1 = new JPanel(); 
-       p2 = new JPanel();     
+       //p2 = new JPanel();     
         
         //p0.setLayout(new GridLayout(1, 1)); 
         //p1.setLayout(new GridLayout(10, 4)); 
         //p2.setLayout(new GridLayout(15, 4)); 
         
        p0.add(Recherche); 
-        p1.add(MaJ); 
-        p1.add(Reporting); 
-        p2.add(Connexion); 
+        p0.add(MaJ); 
+        p0.add(Reporting); 
+        p0.add(Connexion); 
          
       // disposition geographique des panneaux 
         this.getContentPane().add(p0, BorderLayout.NORTH);
-        this.getContentPane().add(p1, BorderLayout.CENTER);
-        this.getContentPane().add(p2, BorderLayout.SOUTH);
+        //this.getContentPane().add(p1, BorderLayout.CENTER);
+        //this.getContentPane().add(p2, BorderLayout.SOUTH);
      // add("North", p0); 
       //  add("Center", p1); 
        // add("South", p2); 
          
          
-  } 
+  }  
+  
+  public Menu(Connection conn, int choix){
+     String nameDatabase=null; 
+     nameDatabase="hopital";
+     namebdd=nameDatabase;
+     //connecte= conn;
+     //initComponents();
+     
+  }
      
  
     @Override 
-    public void actionPerformed(ActionEvent ae) { 
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates. 
-    } 
+    public void actionPerformed(ActionEvent e) { 
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods,choose Tools | Templates. 
+     //Object source = e.getSource();
+     
+     //if (source == Recherche)
+         
+     
+         }
+    
+    
  
     @Override 
     public void itemStateChanged(ItemEvent ie) { 
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates. 
     } 
 } 
+
 
